477b7b307ec76683d38d384f338eaae156c91359
hari <harikrishna92364@gmail.com>
2024-01-26 16:56:45 +0000

    recipe intial commit

---
+++ recipe/src/main/java/com/example/recipe/Recipe.java
@@ -0,0 +1,52 @@
+package com.example.recipe;
+
+import java.util.List;
+
+public class Recipe {
+    private int recipeId;
+    private String recipeName;
+    private String recipeType;
+    private List<String> ingredients;
+
+    // Constructor
+    public Recipe(int recipeId, String recipeName, String recipeType, List<String> ingredients) {
+        this.recipeId = recipeId;
+        this.recipeName = recipeName;
+        this.recipeType = recipeType;
+        this.ingredients = ingredients;
+    }
+
+    // Getter methods
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getRecipeType() {
+        return recipeType;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    // Setter methods
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public void setRecipeType(String recipeType) {
+        this.recipeType = recipeType;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
---
+++ recipe/src/main/java/com/example/recipe/RecipeController.java
@@ -0,0 +1,43 @@
+/*
+ * 
+ * You can use the following import statements
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ * 
+ */
+package com.example.recipe;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+public class RecipeController {
+
+    RecipeService recipeService = new RecipeService();
+
+    @GetMapping("/recipes")
+    public ArrayList<Recipe> getRecipes() {
+        return recipeService.getRecipes();
+    }
+
+    @PostMapping("/recipes")
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
+        return recipeService.addRecipe(recipe);
+    }
+
+    @GetMapping("/recipes/{recipeId}")
+    public Recipe getRecipeById(@PathVariable("recipeId") int recipeId) {
+        return recipeService.getRecipeById(recipeId);
+    }
+
+    @PutMapping("/recipes/{recipeId}")
+    public Recipe updateRecipe(@PathVariable("recipeId") int recipeId, @RequestBody Recipe recipe) {
+        return recipeService.updateRecipe(recipeId, recipe);
+    }
+
+    @DeleteMapping("/recipes/{recipeId}")
+    public void deleteRecipeById(@PathVariable("recipeId") int recipeId) {
+        recipeService.deleteRecipeById(recipeId);
+    }
+
+}
